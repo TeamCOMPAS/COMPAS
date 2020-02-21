6bde5f8a5f68c329a9598e2d1d66e4f76c4883e4
jeffriley <jeffrey.riley@monash.edu>
2020-02-21 11:26:44 +1100

    Defect repairs: (1) fixed issue #31: zRocheLobe function does not use angular momentum loss (2) fixed default logfile path (defaulted to '/' instead of './') (3) changed default CE_ZETA_PRESCRIPTION to SOBERMAN (was STARTRACK which is no longer supported)

--- src/constants.h
+++ src/constants.h
@@ -165,14 +165,18 @@
 //                                          Changed 'MCH' to 'MECS' in 
 //                                             BaseStar::CalculateMaximumCoreMassSN()
 //                                             GiantBranch::CalculateCoreMassAtSupernova_Static
-// 02.05.01      FSB - Jan 27, 2020  -		Enhancement:
-										// Cleaned up default printed headers and parameters constants.h:
-										// - removed double parameters that were printed in multiple output files 
-										// - changed some of the header names to more clear / consistent names
-										// - added some comments in the default printing below for headers that we might want to remove in the near future
+// 02.05.01      FSB - Jan 27, 2020 -Enhancement:
+//                                       Cleaned up default printed headers and parameters constants.h:
+//                                           - removed double parameters that were printed in multiple output files 
+//                                           - changed some of the header names to more clear / consistent names
+//                                           - added some comments in the default printing below for headers that we might want to remove in the near future
+// 02.05.02      JR - Feb 21, 2020 - Defect repairs:
+//                                       - fixed issue #31: zRocheLobe function does not use angular momentum loss
+//                                       - fixed default logfile path (defaulted to '/' instead of './')
+//                                       - changed default CE_ZETA_PRESCRIPTION to SOBERMAN (was STARTRACK which is no longer supported)
 
 
-const std::string VERSION_STRING = "02.05.01";
+const std::string VERSION_STRING = "02.05.02";
 
 
 typedef unsigned long int                                               OBJECT_ID;                  // OBJECT_ID type
