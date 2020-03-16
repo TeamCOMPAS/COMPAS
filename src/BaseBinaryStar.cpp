814104c62c6dc8e561f9177cb1f48e7d89c1c476
Reinhold Willcox <33348824+reinhold-willcox@users.noreply.github.com>
2020-03-16 21:23:04 +1100

    Revert "Jr defect repairs (#127)"
    This reverts commit 1acc396c22c02723e69348bc154c37d43f74570c.

--- src/BaseBinaryStar.cpp
+++ src/BaseBinaryStar.cpp
@@ -360,9 +360,9 @@ void BaseBinaryStar::SetRemainingCommonValues() {
 	m_MassTransferTrackerHistory                 = MT_TRACKING::NO_MASS_TRANSFER;
     m_MassTransfer                               = false;
 
-    m_JLoss                                      = OPTIONS->MassTransferJloss();
+    m_JLoss                                      = DEFAULT_INITIAL_DOUBLE_VALUE;
 
-	m_FractionAccreted                           = OPTIONS->MassTransferFractionAccreted();
+	m_FractionAccreted                           = DEFAULT_INITIAL_DOUBLE_VALUE;
 
     m_FastPhaseCaseA                             = false;
 
@@ -2746,7 +2746,8 @@ void BaseBinaryStar::CalculateMassTransfer(const double p_Dt) {
 	bool qCritFlag = OPTIONS->MassTransferCriticalMassRatioMSLowMass()   || OPTIONS->MassTransferCriticalMassRatioMSHighMass()  ||
 	                 OPTIONS->MassTransferCriticalMassRatioHG()          || OPTIONS->MassTransferCriticalMassRatioGiant()       ||
 	                 OPTIONS->MassTransferCriticalMassRatioHeliumGiant() || OPTIONS->MassTransferCriticalMassRatioHeliumMS()    ||
-                     OPTIONS->MassTransferCriticalMassRatioHeliumHG()    || OPTIONS->MassTransferCriticalMassRatioWhiteDwarf();
+                     OPTIONS->MassTransferCriticalMassRatioHeliumHG()    || OPTIONS->MassTransferCriticalMassRatioHeliumGiant() ||
+                     OPTIONS->MassTransferCriticalMassRatioWhiteDwarf();
 
     if (m_Donor->IsMassRatioUnstable(m_Accretor->Mass(), m_Accretor->IsDegenerate()) && qCritFlag) {
         m_CEDetails.CEEnow = true;
