3233669cf93ccd014ef6dbfc5af39a32cf559c9a
jeffriley <jeffrey.riley@monash.edu>
2020-04-07 10:24:58 +1000

    Defect repair: (1) corrected calculation in return statement for Rand::Random(const double p_Lower, const double p_Upper) (issue #201)

--- src/Rand.cpp
+++ src/Rand.cpp
@@ -78,7 +78,7 @@ double Rand::Random(const double p_Lower, const double p_Upper) {
     double lower = std::min(p_Lower, p_Upper);
     double upper = std::max(p_Lower, p_Upper);
 
-    return (gsl_rng_uniform(m_Rng) + lower) * (upper - lower);
+    return (gsl_rng_uniform(m_Rng) * (upper - lower)) + lower;
 }
 
 
