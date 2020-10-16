249e0c578e09808e06d97dec6515ba777e09bcff
ilyamandel <ilyamandel@gmail.com>
2020-10-16 19:19:24 +1100

    How did GridSamples get added?

--- src/GridSamples.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-samples = (np.random.pareto((2.3-1), 1000) + 1)
-
-count, bins, _ = plt.hist(samples, 100, density=True)
-fit = (2.3-1)/ bins**(2.3)
-plt.plot(bins, max(count)*fit/max(fit), linewidth=2, color='r')
-plt.show()
