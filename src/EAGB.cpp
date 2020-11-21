ddf6049970484a1dcc0318c4f5cd414c1d8effba
Tom Wagg <twagg@college.harvard.edu>
2020-11-21 15:37:51 -0500

    cleanup variable names, add missing rates, change WR winds to always Z dependent

--- src/EAGB.cpp
+++ src/EAGB.cpp
@@ -559,12 +559,11 @@ double EAGB::CalculateCOCoreMassOnPhase(const double p_Time) {
  * @return                                      Mass loss rate in Msol per year
  */
 double EAGB::CalculateMassLossRateHurley() {
-
-    double dms = CalculateMassLossRateNieuwenhuijzenDeJager();
-    double dml = CalculateMassLossRateKudritzkiReimers();
-    double dmt = CalculateMassLossRateVassiliadisWood();
-
-    return std::max(dms, std::max(dml, dmt));
+    double rate_NJ = CalculateMassLossRateNieuwenhuijzenDeJager();
+    double rate_KR = CalculateMassLossRateKudritzkiReimers();
+    double rate_VW = CalculateMassLossRateVassiliadisWood();
+    double rate_WR = CalculateMassLossRateWolfRayetZDependent(m_Mu);
+    return std::max(rate_WR, std::max(rate_NJ, std::max(rate_KR, rate_VW)));
 }
 
 
