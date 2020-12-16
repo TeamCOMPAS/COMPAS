703dc2db0b77f9eb55e23a88dc4d92c05ee46723
Tom Wagg <twagg@college.harvard.edu>
2020-12-16 12:05:22 -0500

    update changelog

--- src/changelog.h
+++ src/changelog.h
@@ -631,7 +631,9 @@
 //                                      - Added a new class variable to track the dominant mass loss rate at each timestep
 // 02.17.13     JR - Dec 11, 2020   - Defect repair
 //                                      - uncomment initialisations of mass transfer critical mass ratios in Options.cpp (erroneously commented in v02.16.00)
+// 02.17.14     TW - Dec 16, 2020   - Bug fix
+//                                      - fix behaviour at fLBV=0 (had been including other winds but should just ignore them)
 
-const std::string VERSION_STRING = "02.17.13";
+const std::string VERSION_STRING = "02.17.14";
 
 # endif // __changelog_h__
