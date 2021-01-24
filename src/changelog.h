f43d6a5cb36f5863b3a223a153a1e08234c90647
Tom Wagg <twagg@college.harvard.edu>
2021-01-24 17:22:34 -0500

    changelog and version

--- src/changelog.h
+++ src/changelog.h
@@ -665,15 +665,17 @@
 //                                      - Added 'hdf5-buffer-size' option - specifies the HDF5 IO buffer size (number of chunks)
 //                                      - Removed 'logfile-delimiter' option - delimiter now set by logfile type (--logfile-type option described above)
 //                                      - Changed header strings containing '/' character: '/' replaced by '|' (header strings become dataset names in HDF5 files, and '/' is a path delimiter...)
-// 02.18.01     SS - Jan 11, 2021 - Defect repair
+// 02.18.01     SS - Jan 11, 2021   - Defect repair
 //                                      - Added check if binary is bound when evolving unbound binaries
 // 02.18.02     JR - Jan 12, 2021   - Defect repair:
 //                                      - Changed "hdf5_chunk_size = 5000" to "hdf5_chunk_size = 100000" in default pythonSubmit (inadvertently left at 5000 after some tests...)
-// 02.18.03     SS - Jan 19, 2021 - Enhancement:
+// 02.18.03     SS - Jan 19, 2021   - Enhancement:
 // 										- Added check for neutron star mass against maximum neutron star mass. 
 //										If a neutron star exceeds this mass it should collapse to a black hole. This can be relevant for neutron stars accreting, e.g. during common envelope evolution
-// 
+// 02.18.04     TW - Jan 24, 2021   - Defect repair:
+//                                      - Fix hubble time mask so that it is cast to booleans instead of used as ints
+//                                      - update hdf5 file group names to new ones
 
-const std::string VERSION_STRING = "02.18.03";
+const std::string VERSION_STRING = "02.18.04";
 
 # endif // __changelog_h__
