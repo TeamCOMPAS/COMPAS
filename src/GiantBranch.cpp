72c7e08f277cc055663ae2ad775c4f6c4dc38828
Lieke van Son <aac.van.son@gmail.com>
2021-04-07 09:36:02 -0400

    moved PPISN boundaries to constants

--- src/GiantBranch.cpp
+++ src/GiantBranch.cpp
@@ -1772,11 +1772,11 @@ STELLAR_TYPE GiantBranch::ResolvePulsationalPairInstabilitySN() {
 
         case PPI_PRESCRIPTION::FARMER: {                                                                // Farmer et al. 2019 http://dx.doi.org/10.3847/1538-4357/ab518b
                                                                                                         // Three cases:
-            if (m_COCoreMass < 38.0){
+            if (m_COCoreMass < FARMER_PPISN_UPP_LIM_LIN_REGIME){
                 m_Mass = m_COCoreMass + 4.;                                                             // A linear relation below CO core masses of 38 Msun
                 }
 
-            else if (m_COCoreMass < 60.0){                                                              // A quadratic relation in CO core mass for 38 =< CO_core < 60
+            else if (m_COCoreMass < FARMER_PPISN_UPP_LIM_QUAD_REGIME){                                  // A quadratic relation in CO core mass for 38 =< CO_core < 60
                 double a1             = -0.096;
                 double a2             = 8.564;
                 double a3             = -2.07;
@@ -1784,7 +1784,7 @@ STELLAR_TYPE GiantBranch::ResolvePulsationalPairInstabilitySN() {
                 m_Mass = a1*pow(m_COCoreMass, 2.0)  + a2*m_COCoreMass + a3*log10(m_Metallicity) + a4  ;
               }
 
-            else if (m_COCoreMass < 140.0){                                                             // No remnant between 60 - 140 Msun
+            else if (m_COCoreMass < FARMER_PPISN_UPP_LIM_INSTABILLITY){                                 // No remnant between 60 - 140 Msun
                 m_Mass = 0;
               }
             else {                                                                                      // BH mass becomes CO-core mass above the PISN gap
