4abce072470a59ef9f18cdbc0b68f3ac67bd6e5e
jeffriley <jeffrey.riley@monash.edu>
2021-08-26 15:34:52 +1000

    Enhancement: Added functionality to allow users to select a range of lines from the grid file (if specified) to process.  Added program options --grid-ftart-line and --grid-lines-to-process - see documentation for details

--- src/changelog.h
+++ src/changelog.h
@@ -765,7 +765,9 @@
 // 02.21.01     RTW - Aug 21, 2021  - Defect Repair:
 //                                      - PrintRLOFProperties now gets called immediately before and after the call to EvaluateBinary so that the changes reflect only BSE changes.
 //                                      - The function call has also been tidied up to take an argument specifying whether the call was made before or after the MT took place.
+// 02.22.00     JR - Aug 26, 2021   - Enhancement:
+//                                      - Added functionality to allow users to select a range of lines from the grid file (if specified) to process.  Added program options --grid-ftart-line and --grid-lines-to-process - see documentation for details.
 
-const std::string VERSION_STRING = "02.21.01";
+const std::string VERSION_STRING = "02.22.00";
 
 # endif // __changelog_h__
