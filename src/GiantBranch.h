bee0f00527a79080f5748095d1735112198e74bf
Reinhold Willcox <reinhold.willcox@mail.mcgill.ca>
2022-02-16 11:10:39 +1100

    fixed TypeIa to TypeIIa in GiantBranch.h

--- src/GiantBranch.h
+++ src/GiantBranch.h
@@ -120,7 +120,7 @@ protected:
             STELLAR_TYPE    ResolveElectronCaptureSN();
             STELLAR_TYPE    ResolvePairInstabilitySN();
             STELLAR_TYPE    ResolvePulsationalPairInstabilitySN();
-            STELLAR_TYPE    ResolveTypeIaSN();
+            STELLAR_TYPE    ResolveTypeIIaSN();
     
             void            UpdateAgeAfterMassLoss() { }                                                                                                                        // NO-OP for most stellar types
 
