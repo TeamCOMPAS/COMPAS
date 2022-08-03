00974166d94f2a8b24ff6e59437a1a86854a1464
Reinhold Willcox <reinhold.willcox@mail.mcgill.ca>
2022-08-03 15:36:28 +1000

    added check for IsSupernova in ONeWD.cpp

--- src/ONeWD.cpp
+++ src/ONeWD.cpp
@@ -14,14 +14,19 @@
 
 STELLAR_TYPE ONeWD::ResolveAIC() { 
 
-    std::cout << "got here" << std::endl; // RTW
+    if (IsSupernova()) {                                                                            // has gone supernova
+        std::cout << "got here" << std::endl; // RTW
 
-    m_Mass       = MECS_REM;                                                            // defined in constants.h
-    m_Radius     = NS::CalculateRadiusOnPhase_Static(m_Mass);                           // neutronStarRadius in Rsol
-    m_Luminosity = NS::CalculateLuminosityOnPhase_Static(m_Mass, m_Age);
-    
-    SetSNCurrentEvent(SN_EVENT::AIC);                                                  // AIC happening now
-    SetSNPastEvent(SN_EVENT::AIC);                                                     // ... and will be a past event
-    
-    return STELLAR_TYPE::NEUTRON_STAR;
+        m_Mass       = MECS_REM;                                                            // defined in constants.h
+        m_Radius     = NS::CalculateRadiusOnPhase_Static(m_Mass);                           // neutronStarRadius in Rsol
+        m_Luminosity = NS::CalculateLuminosityOnPhase_Static(m_Mass, m_Age);
+        
+        SetSNCurrentEvent(SN_EVENT::AIC);                                                  // AIC happening now
+        SetSNPastEvent(SN_EVENT::AIC);                                                     // ... and will be a past event
+        
+        return STELLAR_TYPE::NEUTRON_STAR;
+    } else {
+        return m_StellarType;
+    }
+
 }
