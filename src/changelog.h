d1793babbcba1de7af1e79c6ce356ea0841e474b
Ilya Mandel <ilyamandel@users.noreply.github.com>
2022-08-04 22:18:04 +1000

    Print to DetailedOutput after merger, addresses issue 825 (#830)
    * Print to DetailedOutput after merger, addresses issue 825
    
    * Print to DetailedOutput after merger, addresses issue 825
    
    * Added error message
    
    * Removing spurious comment, adding missing option to defaul .yaml

--- src/changelog.h
+++ src/changelog.h
@@ -914,7 +914,11 @@
 //                                      - Fixed check for non-interacting ECSN progenitors to consider MT history instead of H-richness
 // 02.31.06     RTW - July 25, 2022  - Enhancement:
 //                                      - Added stellar merger to default BSE_RLOF output
+// 02.31.07     IM - August 1, 2022  - Defect repair:
+//                                      - Print to DetailedOutput after merger, addresses https://github.com/TeamCOMPAS/COMPAS/issues/825
+//                                      - Ensures no ONeWDs are formed with masses above Chandrasekhar mass
+//                                      - Minor comment tweaks and a bit of defensive programming
 
-const std::string VERSION_STRING = "02.31.06";
+const std::string VERSION_STRING = "02.31.07";
 
 # endif // __changelog_h__
