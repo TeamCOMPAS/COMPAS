2812e20655b0946650d0fbb3386bb7551bcac672
Reinhold Willcox <reinhold.willcox@mail.mcgill.ca>
2022-08-09 10:06:25 +1000

    removed else in ONeWD.cpp

--- src/ONeWD.cpp
+++ src/ONeWD.cpp
@@ -20,18 +20,15 @@ STELLAR_TYPE ONeWD::ResolveAIC() {
     if (!IsSupernova()) {                                                                            // has gone supernova
         return m_StellarType;
 
-    } else {
+    m_Mass       = MECS_REM;                                                            // defined in constants.h
+    m_Radius     = NS::CalculateRadiusOnPhase_Static(m_Mass);                           // neutronStarRadius in Rsol
+    m_Luminosity = NS::CalculateLuminosityOnPhase_Static(m_Mass, m_Age);
+    
+    m_SupernovaDetails.drawnKickMagnitude = 0.0;
+    m_SupernovaDetails.kickMagnitude      = 0.0;
 
-        m_Mass       = MECS_REM;                                                            // defined in constants.h
-        m_Radius     = NS::CalculateRadiusOnPhase_Static(m_Mass);                           // neutronStarRadius in Rsol
-        m_Luminosity = NS::CalculateLuminosityOnPhase_Static(m_Mass, m_Age);
-        
-        m_SupernovaDetails.drawnKickMagnitude = 0.0;
-        m_SupernovaDetails.kickMagnitude      = 0.0;
+    SetSNCurrentEvent(SN_EVENT::AIC);                                                  // AIC happening now
+    SetSNPastEvent(SN_EVENT::AIC);                                                     // ... and will be a past event
 
-        SetSNCurrentEvent(SN_EVENT::AIC);                                                  // AIC happening now
-        SetSNPastEvent(SN_EVENT::AIC);                                                     // ... and will be a past event
-
-        return STELLAR_TYPE::NEUTRON_STAR;
-    }
+    return STELLAR_TYPE::NEUTRON_STAR;
 }
