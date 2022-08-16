4dba6e362045e0214d8df315a10503434d76d69d
ilyamandel <ilyamandel@gmail.com>
2022-08-16 22:12:10 +1000

    Adding Alvaro's magnitude calculation module

--- src/Star.cpp
+++ src/Star.cpp
@@ -506,6 +506,7 @@ EVOLUTION_STATUS Star::Evolve(const long int p_Id) {
             stepNum++;                                                          // increment step number                                                      
             dt = m_Star->CalculateTimestep() * OPTIONS->TimestepMultiplier();   // calculate new timestep
             EvolveOneTimestep(dt);                                              // evolve for timestep
+            CalculateLambdas(Mass()-CoreMass());
             (void)m_Star->PrintDetailedOutput(m_Id);                            // log record  JR: this should probably be before the star switches type, but this way matches the original code
         }
     }
