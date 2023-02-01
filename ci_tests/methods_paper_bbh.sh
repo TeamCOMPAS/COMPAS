9c25fb377a6e619fd9d078f6dc4c871db2086b76
Mike Lau <mike.lau@monash.edu>
2023-02-01 13:57:43 +1100

    bbh-ci remove test for plotting for now

--- ci_tests/methods_paper_bbh.sh
+++ ci_tests/methods_paper_bbh.sh
@@ -3,4 +3,3 @@
 export COMPAS_ROOT_DIR=${GITHUB_WORKSPACE}
 cd ${GITHUB_WORKSPACE}/utils/examples/methods_paper_plots/detailed_evolution
 python3 runSubmitDemo.py
-python3 plot_detailed_evolution.py
