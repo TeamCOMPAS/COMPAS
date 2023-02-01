4a6265526f74151e7df22806e443dad2b8a03394
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-02-01 11:13:48 +1100

    add more tests

--- py_tests/test_h5view.py
+++ py_tests/test_h5view.py
@@ -5,8 +5,10 @@ Tests that the utils/h5view.py module works as expected.
 import unittest
 import os
 import h5py
-from compas_python_utils import h5view
 
+from compas_python_utils import h5view
+from contextlib import redirect_stdout
+from io import StringIO
 
 class TestH5view(unittest.TestCase):
     def setUp(self) -> None:
@@ -17,6 +19,10 @@ class TestH5view(unittest.TestCase):
         """Test that h5view print_contents doesn't fail"""
         h5name = os.path.join(self.res_dir, "COMPAS_Output.h5")
         h5file = h5py.File(h5name, 'r')
-        h5view.printContents(h5name=h5name, h5file=h5file)
-        ## NOTE: this is a bit of a weak test -- as everything goes to stdout
-        ## ideally the functions should be a bit more modularised...
+        with redirect_stdout(StringIO()) as sout:
+            h5view.printContents(h5name=h5name, h5file=h5file)
+        sout = sout.getvalue()
+        assert sout != ""
+        # This is a pretty weak test atm -- just checks that something is printed,
+        # this test is not actually checking the contents of the output
+
