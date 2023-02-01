4450bd6ac0169d53194a455974b92127b04e584a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-02-01 23:30:03 +1100

    test refactoring

---
+++ py_tests/test_run_submit.py
@@ -0,0 +1,6 @@
+import unittest
+from compas_python_utils.preprocessing import runSubmit
+
+class TestRunSubmit(unittest.TestCase):
+    def test_run_submit(self):
+        self.fail("Not implemented yet")
---
+++ py_tests/test_stroopwafel_interface.py
@@ -0,0 +1,6 @@
+import unittest
+from compas_python_utils.preprocessing import stroopwafelInterface
+
+class TestStroopwafelInterface(unittest.TestCase):
+    def test_run_submit(self):
+        self.fail("Not implemented yet")
