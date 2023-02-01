85256a3049e9ecee222f54d0b60060ddc137f771
Reinhold Willcox <reinhold.willcox@mail.mcgill.ca>
2023-02-01 11:03:20 +1100

    renamed utils to misc and untracked_files to unsupported_utils

--- misc/examples/Tutorials/CosmicIntegration.py
+++ misc/examples/Tutorials/CosmicIntegration.py
@@ -1,14 +1,13 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
