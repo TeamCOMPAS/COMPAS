1317db02ccecd1f2c78aa48588aed8658d2f2bc1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-02-21 18:31:26 +1300

    add runSubmit test

--- py_tests/test_run_submit.py
+++ py_tests/test_run_submit.py
@@ -1,6 +1,21 @@
 import unittest
-from compas_python_utils.preprocessing import runSubmit
+from compas_python_utils.preprocessing.runSubmit import runSubmit
+from pytest_utils import time_func_and_capture_sout
+
 
 class TestRunSubmit(unittest.TestCase):
     def test_run_submit(self):
-        self.fail("Not implemented yet")
+        """ Capture the stdout from runSubmit(execute=False) and check that it is as expected """
+        _, _, sout = time_func_and_capture_sout(runSubmit, execute=False)
+        # test that sout has some of the following kwargs
+        expected_kwargs = [
+            "COMPAS",
+            "debug-to-file",
+            "detailed-output",
+            "quiet",
+        ]
+        for kwarg in expected_kwargs:
+            self.assertTrue(
+                kwarg in sout,
+                f"Expected kwarg {kwarg} not found in sout: {sout}"
+            )
