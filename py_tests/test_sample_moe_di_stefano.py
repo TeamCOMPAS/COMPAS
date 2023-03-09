069e0028ca893728266f5b34796e2ece05876214
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-03-10 11:46:38 +1300

    cleanup test suite

--- py_tests/test_sample_moe_di_stefano.py
+++ py_tests/test_sample_moe_di_stefano.py
@@ -1,21 +1,13 @@
-import unittest
-import shutil, os
-from compas_python_utils.preprocessing.sampleMoeDiStefano import main
+import pytest
+import os
+from compas_python_utils.preprocessing import sampleMoeDiStefano
 
 
-class TestSampleMoeDiStefano(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.outdir = "out_test"
-        os.makedirs(self.outdir, exist_ok=True)
-
-    def tearDown(self) -> None:
-        shutil.rmtree(self.outdir)
-
-    def test_main(self):
-        fname = os.path.join(self.outdir, "gridfn.txt")
-        # This can take ~1 minute to run
-        main(cli_args=['--gridname', fname, '--nSamples', '10'])
-        self.assertTrue(os.path.exists(fname))
-        # TODO: ask reinhold if there is a way to check the contents
-        # TODO: ask reinhold how to allow createParameterDistributionsAndSampler to be run with smaller grid-points
+@pytest.mark.slow
+def test_generate_MoeDiStefano_gridfile(tmp_path):
+    fname = os.path.join(tmp_path, "gridfn.txt")
+    # This can take ~1 minute to run
+    sampleMoeDiStefano.main(cli_args=['--gridname', fname, '--nSamples', '10'])
+    assert os.path.exists(fname)
+    # TODO: ask reinhold if there is a way to check the contents
+    # TODO: ask reinhold how to allow createParameterDistributionsAndSampler to be run with smaller grid-points
