591bc7828f8562c81936641fee98a95663843838
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-04-20 13:59:12 +1200

    add which python check

--- misc/examples/methods_paper_plots/detailed_evolution/run.sh
+++ misc/examples/methods_paper_plots/detailed_evolution/run.sh
@@ -1,4 +1,4 @@
-echo "Run example COMPAS job using the config `example_bbh_compas_config.yaml`"
+echo "Run example COMPAS job using the config example_bbh_compas_config.yaml"
 compas_run_submit example_bbh_compas_config.yaml > example_bbh.log
 cat example_bbh.log
 echo "Generating detailed evolution plot"
