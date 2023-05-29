85d7e39504e7bc94f8b8e5ce269340cc5f8af208
avi <avi.vajpeyi@gmail.com>
2023-05-29 16:55:06 +1200

    add Cpp version of IMF functions

---
+++ compas_python_utils/cosmic_integration/lib/include/integrator/compas_data.h
@@ -0,0 +1,24 @@
+#ifndef _INTEGRATOR_COMPAS_DATA_READER_H_
+#define _INTEGRATOR_COMPAS_DATA_READER_H_
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+namespace integrator{ namespace compas_data_reader{
+    class CompasDataReader {
+        // This class reads the data from a COMPAS HDF5 file
+
+        void read_compas_file(std::string filename);
+
+        public:
+            CompasDataReader(std::string filename);
+            ~CompasDataReader();
+
+        // Getters
+        double get_mass();
+        double get_radius();
+
+
+    }
+}}
