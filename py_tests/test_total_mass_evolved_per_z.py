d8d77d36279e7f85631e5df996eec6f1e7e98d1a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-06-05 09:29:00 +0200

    add placeholder for analytical vs numerical test

--- py_tests/test_total_mass_evolved_per_z.py
+++ py_tests/test_total_mass_evolved_per_z.py
@@ -1,9 +1,12 @@
 from compas_python_utils.cosmic_integration.totalMassEvolvedPerZ import (
-    IMF, get_COMPAS_fraction, analytical_star_forming_mass_per_binary_using_kroupa_imf
+    IMF, get_COMPAS_fraction, analytical_star_forming_mass_per_binary_using_kroupa_imf,
+    star_forming_mass_per_binary
 )
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+MAKE_PLOTS = False
 
 def test_imf(test_archive_dir):
     m = np.geomspace(5e-3, 200, 100)
@@ -14,14 +17,15 @@ def test_imf(test_archive_dir):
     assert imf_values[0] == 0, "IMF(m<min-m) ==0"
     assert np.sum(imf_values < 0) == 0, "IMF must be > 0"
 
-    plt.plot(m, imf_values)
-    for mi in m_breaks:
-        plt.axvline(mi, zorder=-10, color='gray', alpha=0.2)
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.xlabel(r"Mass [M$_{\odot}]$")
-    plt.ylabel("IMF")
-    plt.savefig(f"{test_archive_dir}/IMF.png")
+    if MAKE_PLOTS:
+        plt.plot(m, imf_values)
+        for mi in m_breaks:
+            plt.axvline(mi, zorder=-10, color='gray', alpha=0.2)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel(r"Mass [M$_{\odot}]$")
+        plt.ylabel("IMF")
+        plt.savefig(f"{test_archive_dir}/IMF.png")
 
 
 def test_compas_fraction():
@@ -46,3 +50,13 @@ def test_analytical_function():
         fbin=1
     )
     assert 79.0 < default_case < 79.2
+
+def test_analytical_vs_numerical_star_forming_mass_per_binary():
+    m1_max = 150
+    m1_min = 5
+    m2_min = 0.1
+    fbin = 1
+
+    numerical = star_forming_mass_per_binary(m1_min,m1_max,m2_min,fbin)
+    analytical = analytical_star_forming_mass_per_binary_using_kroupa_imf(m1_min,m1_max,m2_min,fbin)
+    assert np.isclose(numerical,analytical,rtol=0.01)
