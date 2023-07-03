e068ccb9d976a9d1e45f2cc23b0671f99b02a560
avi <avi.vajpeyi@gmail.com>
2023-07-03 17:38:29 +1200

    remove hardcoded kernel name

--- utils/examples/CosmicIntegration.py
+++ utils/examples/CosmicIntegration.py
@@ -7,10 +7,6 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.7
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
 # ---
 
 # %matplotlib inline
