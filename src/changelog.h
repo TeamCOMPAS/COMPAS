faeeafccbc0354850321212c9f76a327fc1ff79c
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-03-21 22:37:52 +0100

    added explicit definition for isUnstable, updated changelog

--- src/changelog.h
+++ src/changelog.h
@@ -1109,7 +1109,9 @@
 //                                      - calling code for MassLossToFitInsideRocheLobe() and Mass0ToMatchDesiredCoreMass() now handles -ve return:
 //                                           - if MassLossToFitInsideRocheLobe() returns -ve value (i.e. no root found), the binary immediately enters a CE phase
 //                                           - if Mass0ToMatchDesiredCoreMass() returns -ve value (i.e. no root found), an arbitrary value is used for core mass (see code for value)
+// 02.42.02    RTW - Mar 21, 2024    - Minor defect repair
+//                                      - Added explicit definition `bool isUnstable = false` to avoid confusion in BaseBinaryStar.cpp
 
-const std::string VERSION_STRING = "02.42.01";
+const std::string VERSION_STRING = "02.42.02";
 
 # endif // __changelog_h__
