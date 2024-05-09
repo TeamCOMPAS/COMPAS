7478b9d2caa3c9d2fb8a0d9967e438d7e15e603f
Jeff Riley <jeff.riley@monash.edu>
2024-05-09 17:23:18 +1000

    wip

--- online-docs/conf.py
+++ online-docs/conf.py
@@ -29,7 +29,7 @@ author = 'TeamCOMPAS'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-#needs_sphinx = '4.3.2'
+needs_sphinx = '4.3.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
