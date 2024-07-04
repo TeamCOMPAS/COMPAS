1cd6c888160d8c568e355fd6e5c81a1fafd74a5d
Ilya Mandel <ilyamandel@users.noreply.github.com>
2024-07-05 00:21:30 +0200

    Update BaseBinaryStar.cpp
    comment update

--- src/BaseBinaryStar.cpp
+++ src/BaseBinaryStar.cpp
@@ -1862,7 +1862,7 @@ double BaseBinaryStar::CalculateGammaAngularMomentumLoss_Static(const double p_D
             case MT_ANGULAR_MOMENTUM_LOSS_PRESCRIPTION::JEANS                : gamma = p_AccretorMass / p_DonorMass; break;                     // vicinity of the donor
             case MT_ANGULAR_MOMENTUM_LOSS_PRESCRIPTION::ISOTROPIC_RE_EMISSION: gamma = p_DonorMass / p_AccretorMass; break;                     // vicinity of the accretor
             case MT_ANGULAR_MOMENTUM_LOSS_PRESCRIPTION::CIRCUMBINARY_RING    : 
-                gamma = (M_SQRT2 * (p_DonorMass + p_AccretorMass) * (p_DonorMass + p_AccretorMass)) / (p_DonorMass * p_AccretorMass); break;    // based on the assumption that a_ring ~= 2*a*, Vinciguerra+, 2020 
+                gamma = (M_SQRT2 * (p_DonorMass + p_AccretorMass) * (p_DonorMass + p_AccretorMass)) / (p_DonorMass * p_AccretorMass); break;    // based on the assumption that a_ring ~= 2*a, Vinciguerra+, 2020 
             case MT_ANGULAR_MOMENTUM_LOSS_PRESCRIPTION::MACLEOD_LINEAR       : {                                                                // linear interpolation on separation between accretor and L2 point
                 double q = p_AccretorMass / p_DonorMass;
                 // interpolate in separation between a_acc and a_L2, both normalized to units of separation a
