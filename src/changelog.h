c73a3a5f169446b3afa7f98b6d2c074468641bc5
Jeff Riley <jeff.riley@monash.edu>
2024-08-30 11:59:02 -0600

    added deprecation notice (missed in v03.00.00)

--- src/changelog.h
+++ src/changelog.h
@@ -1276,7 +1276,6 @@
 //                                      - Remove extraneous references to "kick_direction" in LogTypedefs.h (added in error in v03.00.00)
 // 03.01.04   SS - Aug 28, 2024     - Enhancement:
 //                                      - Add Hendriks+23 pulsational pair instability prescription 
-//
 // 03.01.05   JDM - Aug 30, 2024    - Defect repair, minor cleanup:
 //                                      - Related to issue #502: added HG to allowed RSG stellar type check, preventing GB winds from being applied during HG.
 //                                      - Changed all "FLEXIBLE2023" naming to "MERRITT2024"
@@ -1285,6 +1284,7 @@
 //                                        New option: --include-WD-binaries-as-DCO
 //                                        See "What's New" and option documentation for details.
 //                                        (Issue #1170)
+//                                      - added deprecation notice for '--mass-loss-prescription NONE' (should use ZERO) - missed in v03.00.00
 
 
 const std::string VERSION_STRING = "03.01.06";
