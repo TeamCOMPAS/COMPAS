e90d8e6e20b93627046f191a6983a90ea48b134b
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-11-18 18:45:20 +0100

    changelog again

--- src/changelog.h
+++ src/changelog.h
@@ -1376,7 +1376,9 @@
 // 03.07.05   IM - Nov 07, 2024     - Defect repairs:
 //                                      - Fix for issue #1270: root finder functions now check if either of the bracket edges provides a sufficiently good solution, which sometimes happens when the initial guess is very close to the truth
 // 03.08.00  RTW - Nov 18, 2024     - Enhancement:
-//                                      - Added new critical mass ratio tables for He stars from Ge et al. team, now all of their Papers I-V are included
+//                                      - Added new critical mass ratio tables for He stars from Ge et al. team
+//                                      - Cleaned up the stability calculation for H-rich stars as well, specifically implementing nearest neighbor for extrapolation
+//                                      - Now all of their results from Papers I-V are included (including those requested in private comm.)
 
 const std::string VERSION_STRING = "03.08.00";
 
