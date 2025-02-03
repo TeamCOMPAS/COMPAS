7b6eddfd1cf5f5ff84691ae6260e0443bbfe9267
Adam Brcek <brcekadam@gmail.com>
2025-02-03 14:46:13 +1100

    Fixed stellar tracks for lower mass stars

--- src/MainSequence.h
+++ src/MainSequence.h
@@ -75,7 +75,8 @@ protected:
     double          CalculateLuminosityAtPhaseEnd() const                                   { return CalculateLuminosityAtPhaseEnd(m_Mass0); }                      // Use class member variables
     double          CalculateLuminosityOnPhase(const double p_Time, const double p_Mass, const double p_LZAMS) const;
     double          CalculateLuminosityOnPhase() const                                      { return CalculateLuminosityOnPhase(m_Age, m_Mass0, m_LZAMS0); }        // Use class member variables
-    double          CalculateLuminosityBrcek(const double p_CoreMass, const double p_HeliumAbundanceCore, const double p_Age) const;
+    double          CalculateLuminosityShikauchi(const double p_CoreMass, const double p_HeliumAbundanceCore) const;
+    double          CalculateLuminosityTransitionToHG(const double p_Mass, const double p_Age, double const p_LZAMS) const;
     DBL_DBL         CalculateMainSequenceCoreMassBrcek(const double p_Dt, const double p_MassLossRate);
     double          CalculateInitialMainSequenceCoreMass(const double p_MZAMS) const;
     double          CalculateMomentOfInertia() const                                        { return (0.1 * (m_Mass) * m_Radius * m_Radius); }                      // k2 = 0.1 as defined in Hurley et al. 2000, after eq 109
