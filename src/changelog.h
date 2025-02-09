e39a27a7a063679fe69a0ddfc40ec02d43c8ea5b
Adam Brcek <brcekadam@gmail.com>
2025-02-10 08:49:16 +1100

    Resolve merge conflict

--- src/changelog.h
+++ src/changelog.h
@@ -1437,7 +1437,7 @@
 //                                      - Moved all CHE rotation related code to ProcessTides(), ensuring that any spin up during binary evolution conserves total angular momentum.
 // 03.12.00   AB - Jan 16, 2025     - Enhancement:
 //                                      - Added Shikauchi et al. (2024) core mass prescription, describing convective core evolution under mass loss/gain
-//                                      - New options: --main-sequence-core-mass-prescription BRCEK (new prescription), MANDEL (replaces --retain-core-mass-during-caseA-mass-transfer),
+//                                      - New options: --main-sequence-core-mass-prescription SHIKAUCHI (new prescription), MANDEL (replaces --retain-core-mass-during-caseA-mass-transfer),
 //                                        ZERO (main sequence core mass set to zero, no treatment)
 //                                      - Updated stellar tracks with added luminosity prescription for main sequence stars from Shikauchi et al. (2024)
 //                                      - Added treatment for rejuvenation of main sequence accretors when the new prescription is used
@@ -1452,10 +1452,11 @@
 // 03.12.04   IM - Feb 08, 2025     - Enhancement:
 //                                      - only reset mass0 to mass on the HG when mass0 > mass (i.e., on mass loss, not mass gain, and not if mass0 is intentionally set to yield a lower core mass as may be required by the BRCEK rejuvenation prescription)
 //                                      - replaced name of COMPAS PPISN prescription with WOOSLEY (issue #1278)
+// 03.12.05   AB - Feb 10, 2025     - Enhancement:
+//                                      - SHIKAUCHI option for main sequence core mass renamed to BRCEK
+//                                      - allowed main sequence core mass calculations for lower mass stars and replaced ODEs
+//                                      - always update Mass0 in HG.h when BRCEK prescription is used
 
-
-
-
-const std::string VERSION_STRING = "03.12.04";
+const std::string VERSION_STRING = "03.12.05";
 
 # endif // __changelog_h__
