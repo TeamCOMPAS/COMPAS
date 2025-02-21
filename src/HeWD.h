51e4e9a81d9048830db784677766d1877eb28502
jeffriley <jeffrey.riley@monash.edu>
2025-02-22 09:39:52 +1100

    Update HeWD.h
    Formatting; variable name consistency

--- src/HeWD.h
+++ src/HeWD.h
@@ -52,7 +52,7 @@ protected:
         m_HeShell                  = 0.0;                               // Initialize Helium Shell
         m_l0Ritter                 = Calculatel0Ritter();
         m_XRitter                  = CalculateXRitter();
-        m_lambdaRitter             = CalculatelambdaRitter();
+        m_LambdaRitter             = CalculateLambdaRitter();
         m_IsSubChandrasekharTypeIa = false; 
         m_ShouldRejuvenate         = false;
         m_AccretionRegime          = ACCRETION_REGIME::ZERO;
@@ -62,11 +62,11 @@ protected:
 
 
     // member functions - alphabetically
-    double          CalculateHeliumAbundanceCoreOnPhase() const                                             { return 1.0 - m_Metallicity; };
-    double          CalculateHeliumAbundanceSurfaceOnPhase() const                                          { return 1.0 - m_Metallicity; };
+    double          CalculateHeliumAbundanceCoreOnPhase() const                                             { return 1.0 - m_Metallicity; }
+    double          CalculateHeliumAbundanceSurfaceOnPhase() const                                          { return 1.0 - m_Metallicity; }
     
-    double          CalculateHydrogenAbundanceCoreOnPhase() const                                           { return 0.0; };
-    double          CalculateHydrogenAbundanceSurfaceOnPhase() const                                        { return 0.0; };
+    double          CalculateHydrogenAbundanceCoreOnPhase() const                                           { return 0.0; }
+    double          CalculateHydrogenAbundanceSurfaceOnPhase() const                                        { return 0.0; }
     
     double          CalculateLambdaDewi() const                                                             { return BaseStar::CalculateLambdaDewi(); }
     double          CalculateLambdaNanjingStarTrack(const double p_Mass, const double p_Metallicity) const  { return BaseStar::CalculateLambdaNanjingStarTrack(0.0, 0.0); }
