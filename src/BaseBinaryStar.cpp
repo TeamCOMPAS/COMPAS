3edf94dec12ea484528263ae01c1a661f92d12ee
jeffriley <jeff.riley@monash.edu>
2025-04-13 08:43:38 +1000

    Update BaseBinaryStar.cpp
    Formatting

--- src/BaseBinaryStar.cpp
+++ src/BaseBinaryStar.cpp
@@ -2153,16 +2153,15 @@ void BaseBinaryStar::CalculateMassTransfer(const double p_Dt) {
         }
     }
     
-	// Check for recycled pulsars. Not considering CEE as a way of recycling NSs.
-	if (!m_CEDetails.CEEnow && m_Accretor->IsOneOf({ STELLAR_TYPE::NEUTRON_STAR })) {                                           // accretor is a neutron star, system is not in CE 
+    // Check for recycled pulsars. Not considering CEE as a way of recycling NSs.
+    if (!m_CEDetails.CEEnow && m_Accretor->IsOneOf({ STELLAR_TYPE::NEUTRON_STAR })) {                                           // accretor is a neutron star, system is not in CE 
         m_Donor->SetRLOFOntoNS();                                                                                               // donor donated mass to a neutron star
         m_Accretor->SetRecycledNS();                                                                                            // accretor is (was) a recycled NS
-	}
+    }
     else if (m_CEDetails.CEEnow && OPTIONS->NeutronStarAccretionInCE() != NS_ACCRETION_IN_CE::ZERO) {                           // accretor is a neutron star, system is in CE
         m_Donor->SetCEOntoNS();                                                                                                 // donor donated mass to a neutron star
         m_Accretor->SetRecycledNS();                                                                                            // accretor is (was) a recycled NS
     }
-    
 }
 
 
