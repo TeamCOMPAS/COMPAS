239f917bf1fb2cb7008ee535e59d0bae60d52225
Veome Kapil <veomekapil@gmail.com>
2025-05-06 09:49:42 -0400

    Corrected all l=2 terms in Equilibrium and Dynamical tides, renamed the orbital wavenumber to n for clarity, updated option name to KAPIL2025

--- src/Star.h
+++ src/Star.h
@@ -170,9 +170,9 @@ public:
     
     double          CalculateEddyTurnoverTimescale()                                                                { return m_Star->CalculateEddyTurnoverTimescale(); }
     
-    DBL_DBL_DBL_DBL CalculateImKlmDynamical(const double p_Omega, const double p_SemiMajorAxis, const double p_M2)  { return m_Star->CalculateImKlmDynamical(p_Omega, p_SemiMajorAxis, p_M2); }
-    DBL_DBL_DBL_DBL CalculateImKlmEquilibrium(const double p_Omega, const double p_SemiMajorAxis, const double p_M2){ return m_Star->CalculateImKlmEquilibrium(p_Omega, p_SemiMajorAxis, p_M2); }
-    DBL_DBL_DBL_DBL CalculateImKlmTidal(const double p_Omega, const double p_SemiMajorAxis, const double p_M2)      { return m_Star->CalculateImKlmTidal(p_Omega, p_SemiMajorAxis, p_M2); }
+    DBL_DBL_DBL_DBL CalculateImKnmDynamical(const double p_Omega, const double p_SemiMajorAxis, const double p_M2)  { return m_Star->CalculateImKnmDynamical(p_Omega, p_SemiMajorAxis, p_M2); }
+    DBL_DBL_DBL_DBL CalculateImKnmEquilibrium(const double p_Omega, const double p_SemiMajorAxis, const double p_M2){ return m_Star->CalculateImKnmEquilibrium(p_Omega, p_SemiMajorAxis, p_M2); }
+    DBL_DBL_DBL_DBL CalculateImKnmTidal(const double p_Omega, const double p_SemiMajorAxis, const double p_M2)      { return m_Star->CalculateImKnmTidal(p_Omega, p_SemiMajorAxis, p_M2); }
 
     DBL_DBL         CalculateMassAcceptanceRate(const double p_DonorMassRate, 
                                                 const double p_AccretorMassRate,
