0f2e47eeeba7497b9ac1483d748dd612c6e18322
Ilya Mandel <ilya.mandel@monash.edu>
2025-05-09 21:00:00 +1000

    Typo fix

--- src/changelog.h
+++ src/changelog.h
@@ -1552,7 +1552,7 @@
 //  03.18.06    IM - May 9, 2025    - Enhancement, defect repair:
 //                                      - Added several outputs to provide more information and aid code testing: SEMI_MAJOR_AXIS_POST_STAGE_1_CE (for the 2-stage CE); BINDING_ENERGY_CONVECTIVE_ENVELOPE, CONVECTIVE_ENV_MASS, LAMBDA_CONVECTIVE_ENVELOPE (convective envelope binding energy,  mass, and lambda, respectively, Picker prescription)
 //                                      - Ensure that the MANDEL main sequence core mass prescription never provides a core mass exceeding the total mass
-//                                      - Corrected several erroneous comments, cleaned up a few function defintions
+//                                      - Corrected several erroneous comments, cleaned up a few function definitions
 
 const std::string VERSION_STRING = "03.18.06";
 
