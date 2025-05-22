2163abe025cb647c550630e8c119cdace6a43af3
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-05-22 14:51:09 +1200

    add different stellar types for binned cosmic integrator

--- compas_python_utils/cosmic_integration/binned_cosmic_integrator/binary_population.py
+++ compas_python_utils/cosmic_integration/binned_cosmic_integrator/binary_population.py
@@ -9,15 +9,28 @@ from ..totalMassEvolvedPerZ import (
 )
 from .conversions import m1_m2_to_chirp_mass, m1_m2_to_eta
 from .plotting import plot_binary_population
+from .stellar_type import BH, NS, WD
 
 # Default IMF limits
 M1_MIN = 5
 M1_MAX = 150
 M2_MIN = 0.1
 
+DCO_GROUPS = dict(
+    BBH=[BH, BH],
+    BNS=[NS, NS],
+    BWD=[WD, WD],
+    NSBH=[NS, BH],
+    WDNS=[WD, NS],
+    WDBH=[WD, BH],
+)
+
+VALID_DCO_TYPES = list(DCO_GROUPS.keys())
+
+
 class BinaryPopulation:
     """
-    General DCO population class supporting BBH, BNS, BHNS.
+    General DCO population class supporting BBH, BNS, NSBH.
 
     Loads the DCOs population from COMPAS output.
     Requires the COMPAS output to contain the following datasets:
@@ -40,32 +53,28 @@ class BinaryPopulation:
     - BSE_Double_Compact_Objects/Stellar_Type(2)
     - BSE_Double_Compact_Objects/Merges_Hubble_Time
 
-
-
-    Flags:
-      include_bbh: bool
-      include_bns: bool
-      include_bhns: bool
     """
+
     def __init__(
-        self,
-        m1: np.ndarray,
-        m2: np.ndarray,
-        t_delay: np.ndarray,
-        z_zams: np.ndarray,
-        n_systems: int,
-        include_bbh: bool = True,
-        include_bns: bool = False,
-        include_bhns: bool = False,
-        m1_min: float = M1_MIN,
-        m1_max: float = M1_MAX,
-        m2_min: float = M2_MIN,
-        binary_fraction: float = 0.7,
+            self,
+            m1: np.ndarray,
+            m2: np.ndarray,
+            t_delay: np.ndarray,
+            z_zams: np.ndarray,
+            n_systems: int,
+            dcos_included: List[str],
+            m1_min: float = M1_MIN,
+            m1_max: float = M1_MAX,
+            m2_min: float = M2_MIN,
+            binary_fraction: float = 0.7,
     ):
         # Population selection
-        self.include_bbh = include_bbh
-        self.include_bns = include_bns
-        self.include_bhns = include_bhns
+        self.dcos_included = dcos_included
+        if not any([x in VALID_DCO_TYPES for x in dcos_included]):
+            raise ValueError(
+                f"Invalid DCO types: {dcos_included}. "
+                f"Valid types are: {VALID_DCO_TYPES}"
+            )
 
         # IMF parameters
         self.m1_min = m1_min
@@ -88,17 +97,15 @@ class BinaryPopulation:
 
     @classmethod
     def from_compas_h5(
-        cls,
-        path: str,
-        include_bbh: bool = True,
-        include_bns: bool = False,
-        include_bhns: bool = False,
-        m1_min: float = M1_MIN,
-        m1_max: float = M1_MAX,
-        m2_min: float = M2_MIN,
-        binary_fraction: float = 0.7,
+            cls,
+            path: str,
+            dcos_included: List[str] = ["BBH"],
+            m1_min: float = M1_MIN,
+            m1_max: float = M1_MAX,
+            m2_min: float = M2_MIN,
+            binary_fraction: float = 0.7,
     ) -> "BinaryPopulation":
-        mask = cls._generate_mask(path, include_bbh, include_bns, include_bhns)
+        mask = cls._generate_mask(path, dcos_included)
 
         m1, m2, t0, tC, seeds = _load_data(
             path,
@@ -121,9 +128,7 @@ class BinaryPopulation:
             t_delay=t0 + tC,
             z_zams=z_zams[dco_mask],
             n_systems=len(all_seeds),
-            include_bbh=include_bbh,
-            include_bns=include_bns,
-            include_bhns=include_bhns,
+            dcos_included=dcos_included,
             m1_min=m1_min,
             m1_max=m1_max,
             m2_min=m2_min,
@@ -132,34 +137,20 @@ class BinaryPopulation:
 
     @staticmethod
     def _generate_mask(
-        path: str,
-        include_bbh: bool,
-        include_bns: bool,
-        include_bhns: bool,
+            path: str,
+            dcos_included: List[str],
     ) -> xp.ndarray:
-        # Load fundamental DCO variables
-        t1, t2, hubble_flag, dco_seeds = _load_data(
+        type_mask = _generate_dco_mask(path, dcos_included)
+
+        # Load the Hubble time flag + BSE seeds
+        hubble_flag, dco_seeds = _load_data(
             path,
             "BSE_Double_Compact_Objects",
-            ["Stellar_Type(1)", "Stellar_Type(2)", "Merges_Hubble_Time", "SEED"],
+            ["Merges_Hubble_Time", "SEED"],
         )
-        # Base masks by type
-        masks = []
-        if include_bbh:
-            masks.append((t1 == 14) & (t2 == 14))
-        if include_bns:
-            masks.append((t1 == 13) & (t2 == 13))
-        if include_bhns:
-            masks.append((t1 == 14) & (t2 == 13) | (t1 == 13) & (t2 == 14))
 
-        if not masks:
-            raise ValueError("At least one DCO type must be included.")
-
-
-        type_mask = np.logical_or.reduce(masks)
         # Hubble time filter
         hubble_mask = hubble_flag.astype(bool)
-
 
         # get the flags and unique seeds from the Common Envelopes file
         ce_seeds, rlof_flag, optimistic_ce = _load_data(
@@ -190,7 +181,6 @@ class BinaryPopulation:
         assert len(set(lens.values())) == 1, f"Length mismatch in masks: {lens}"
 
         return type_mask * hubble_mask * mask_out_with_rlof_seeds * mask_out_optimistic_ce_seeds
-
 
     @property
     def n_dcos(self) -> int:
@@ -238,9 +228,7 @@ class BinaryPopulation:
             t_delay=self.t_delay[idx],
             z_zams=self.z_zams[idx],
             n_systems=self.n_systems,
-            include_bbh=self.include_bbh,
-            include_bns=self.include_bns,
-            include_bhns=self.include_bhns,
+            dcos_included=self.dcos_included,
             m1_min=self.m1_min,
             m1_max=self.m1_max,
             m2_min=self.m2_min,
@@ -258,7 +246,32 @@ class BinaryPopulation:
         return self.__repr__()
 
 
-# Helper I/O function remains unchanged
+def _generate_dco_mask(
+        compas_path: str,
+        dcos_included: List[str]
+) -> xp.ndarray:
+    # Load fundamental DCO variables
+    t1, t2 = _load_data(
+        compas_path,
+        "BSE_Double_Compact_Objects",
+        ["Stellar_Type(1)", "Stellar_Type(2)"],
+    )
+
+    masks = []
+    for dco in dcos_included:
+        if dco not in DCO_GROUPS:
+            continue
+        a, b = DCO_GROUPS[dco]
+        # check if t1 in set 'a' of stellar types and t2 in set 'b' of stellar types
+        masks.append((np.isin(t1, a) & np.isin(t2, b)) | (np.isin(t1, b) & np.isin(t2, a)))
+
+    if not masks:
+        raise ValueError("At least one DCO type must be included.")
+
+    # Combine all masks using logical OR
+    type_mask = np.logical_or.reduce(masks)
+    return type_mask
+
 
 def _load_data(path: str, group: str, var_names: List[str], mask: Optional[xp.ndarray] = None):
     with h5py.File(path, "r") as f:
@@ -267,17 +280,18 @@ def _load_data(path: str, group: str, var_names: List[str], mask: Optional[xp.nd
         data = [d[mask] for d in data]
     return data
 
+
 # Mock generation utility
 
 def generate_mock_population(
-    filename: str = "",
-    n_systems: int = 2000,
-    frac_bbh: float = 0.7,
-    frac_bns: float = 0.2,
-    frac_bhns: float = 0.1,
-    m1_min: float = M1_MIN,
-    m1_max: float = M1_MAX,
-    m2_min: float = M2_MIN,
+        filename: str = "",
+        n_systems: int = 2000,
+        frac_bbh: float = 0.7,
+        frac_bns: float = 0.2,
+        frac_bhns: float = 0.1,
+        m1_min: float = M1_MIN,
+        m1_max: float = M1_MAX,
+        m2_min: float = M2_MIN,
 ):
     if filename == "":
         filename = "dco_mock_population.h5"
@@ -287,19 +301,18 @@ def generate_mock_population(
     n_systems = len(m1)
     n_dcos = n_systems // 2
     n_ce = n_systems * 2
-    types = np.random.choice(["BBH","BNS","BHNS"], size=n_dcos,
+    types = np.random.choice(["BBH", "BNS", "NSBH"], size=n_dcos,
                              p=[frac_bbh, frac_bns, frac_bhns])
 
     # Define the type-to-mass mapping
     type_to_pair = {
         "BBH": [14, 14],
         "BNS": [13, 13],
-        "BHNS": [13, 14]
+        "NSBH": [13, 14]
     }
 
     # Create a 2D array by mapping each type to its corresponding mass pair
     mass_pairs = np.array([type_to_pair[t] for t in types]).T
-
 
     # create file structure
     with h5py.File(filename, "w") as f:
@@ -308,14 +321,14 @@ def generate_mock_population(
         f.create_group("BSE_Double_Compact_Objects")
         seeds = np.arange(n_systems)
         f["BSE_System_Parameters"].create_dataset("SEED", data=seeds)
-        f["BSE_System_Parameters"].create_dataset("Metallicity@ZAMS(1)", data=np.random.uniform(1e-4,1e-2,n_systems))
+        f["BSE_System_Parameters"].create_dataset("Metallicity@ZAMS(1)", data=np.random.uniform(1e-4, 1e-2, n_systems))
         f["BSE_System_Parameters"].create_dataset("Mass@ZAMS(1)", data=m1)
         f["BSE_System_Parameters"].create_dataset("Mass@ZAMS(2)", data=m2)
         # CE
         ce_seeds = np.arange(n_ce)
         f["BSE_Common_Envelopes"].create_dataset("SEED", data=ce_seeds)
-        f["BSE_Common_Envelopes"].create_dataset("Immediate_RLOF>CE", data=np.zeros(n_ce,dtype=bool)) # no RLOF after CE
-        f["BSE_Common_Envelopes"].create_dataset("Optimistic_CE", data=np.zeros(n_ce,dtype=bool))  # no optimistic CE
+        f["BSE_Common_Envelopes"].create_dataset("Immediate_RLOF>CE", data=np.zeros(n_ce, dtype=bool))  # no RLOF after CE
+        f["BSE_Common_Envelopes"].create_dataset("Optimistic_CE", data=np.zeros(n_ce, dtype=bool))  # no optimistic CE
         # DCOs
         dco_seeds = np.arange(n_dcos)
         f["BSE_Double_Compact_Objects"].create_dataset("Stellar_Type(1)", data=mass_pairs[0, :])
@@ -323,7 +336,7 @@ def generate_mock_population(
         f["BSE_Double_Compact_Objects"].create_dataset("SEED", data=dco_seeds)
         f["BSE_Double_Compact_Objects"].create_dataset("Mass(1)", data=m1[:n_dcos])
         f["BSE_Double_Compact_Objects"].create_dataset("Mass(2)", data=m2[:n_dcos])
-        f["BSE_Double_Compact_Objects"].create_dataset("Time", data=np.random.uniform(4,13.8,n_dcos))
-        f["BSE_Double_Compact_Objects"].create_dataset("Coalescence_Time", data=np.random.uniform(0,14000,n_dcos))
-        f["BSE_Double_Compact_Objects"].create_dataset("Merges_Hubble_Time", data=np.ones(n_dcos,dtype=bool))
+        f["BSE_Double_Compact_Objects"].create_dataset("Time", data=np.random.uniform(4, 13.8, n_dcos))
+        f["BSE_Double_Compact_Objects"].create_dataset("Coalescence_Time", data=np.random.uniform(0, 14000, n_dcos))
+        f["BSE_Double_Compact_Objects"].create_dataset("Merges_Hubble_Time", data=np.ones(n_dcos, dtype=bool))
     return filename
