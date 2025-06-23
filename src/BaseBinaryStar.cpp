542343685617d51ad812b33ba155588e7205e392
Adam Brcek <brcekadam@gmail.com>
2025-06-23 14:45:03 +1000

    Fix to hydrogen mass calculations

--- src/BaseBinaryStar.cpp
+++ src/BaseBinaryStar.cpp
@@ -1779,13 +1779,13 @@ void BaseBinaryStar::ResolveMainSequenceMerger() {
         (utils::Compare(m_Star2->MZAMS(), BRCEK_LOWER_MASS_LIMIT) >= 0)) {
         
         // total hydrogen masses (from the core, envelope, and the intermediate region between the core and envelope)
-        double hydrogenMass1 = m_Star1->HydrogenAbundanceCore() * m_Star1->MainSequenceCoreMass() + (m_Star1->HydrogenAbundanceCore() + initialHydrogenFraction) * (m_Star1->InitialMainSequenceCoreMass() - m_Star1->MainSequenceCoreMass()) / 2.0 + initialHydrogenFraction * (m_Star1->Mass() - m_Star1->InitialMainSequenceCoreMass());
-        double hydrogenMass2 = m_Star2->HydrogenAbundanceCore() * m_Star2->MainSequenceCoreMass() + (m_Star2->HydrogenAbundanceCore() + initialHydrogenFraction) * (m_Star2->InitialMainSequenceCoreMass() - m_Star2->MainSequenceCoreMass()) / 2.0 + initialHydrogenFraction * (m_Star2->Mass() - m_Star2->InitialMainSequenceCoreMass());
+        double hydrogenMass1 = m_Star1->HydrogenAbundanceCore() * m_Star1->MainSequenceCoreMass() + (m_Star1->HydrogenAbundanceCore() + m_Star1->HydrogenAbundanceSurface()) * (std::min(m_Star1->InitialMainSequenceCoreMass(), m_Star1->Mass()) - m_Star1->MainSequenceCoreMass()) / 2.0 + initialHydrogenFraction * std::max(m_Star1->Mass() - m_Star1->InitialMainSequenceCoreMass(), 0.0);
+        double hydrogenMass2 = m_Star2->HydrogenAbundanceCore() * m_Star2->MainSequenceCoreMass() + (m_Star2->HydrogenAbundanceCore() + m_Star2->HydrogenAbundanceSurface()) * (std::min(m_Star2->InitialMainSequenceCoreMass(), m_Star2->Mass()) - m_Star2->MainSequenceCoreMass()) / 2.0 + initialHydrogenFraction * std::max(m_Star2->Mass() - m_Star1->InitialMainSequenceCoreMass(), 0.0);
         
-        // final hydrogen mass in the merger product, assuming that the lost mass comes from the envelope
-        finalHydrogenMass      = hydrogenMass1 + hydrogenMass2 - (m_Star1->Mass() + m_Star2->Mass() - finalMass) * initialHydrogenFraction;
+        // final hydrogen mass in the merger product, assuming that the lost mass comes from the envelope of the star with higher surface hydrogen abundance
+        finalHydrogenMass = hydrogenMass1 + hydrogenMass2 - (m_Star1->Mass() + m_Star2->Mass() - finalMass) * std::max(m_Star1->HydrogenAbundanceSurface(), m_Star2->HydrogenAbundanceSurface());
     }
-    else finalHydrogenMass     = finalMass * initialHydrogenFraction - tau1 * TAMSCoreMass1 * initialHydrogenFraction - tau2 * TAMSCoreMass2 * initialHydrogenFraction;
+    else finalHydrogenMass = finalMass * initialHydrogenFraction - tau1 * TAMSCoreMass1 * initialHydrogenFraction - tau2 * TAMSCoreMass2 * initialHydrogenFraction;
        
     m_SemiMajorAxis = std::numeric_limits<float>::infinity();                                   // set separation to infinity to avoid subsequent fake interactions with a massless companion (RLOF, CE, etc.)
     
