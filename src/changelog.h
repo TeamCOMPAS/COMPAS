b11e0011c786918d71619c4ffadc09dd9febfa94
Ilya Mandel <ilya.mandel@monash.edu>
2025-06-24 16:22:47 +1000

    Refer to issue in changelog

--- src/changelog.h
+++ src/changelog.h
@@ -1587,7 +1587,7 @@
 //                                          - Fixes to MS mergers and CHE when BRCEK core mass prescription is used -- MS core mass is now correctly initialised after full mixing in MS
 //                                            mergers and CH stars that spun down
 //  03.20.04   IM - June 24, 2025       - Defect repair:
-//                                          - Fixed typo in implementation of CASE_BB_STABILITY_PRESCRIPTION::ALWAYS_STABLE_ONTO_NSBH
+//                                          - Fixed typo in implementation of CASE_BB_STABILITY_PRESCRIPTION::ALWAYS_STABLE_ONTO_NSBH (issue #1403)
 
 
 const std::string VERSION_STRING = "03.20.05";
