0007c69008515a89c1bc383c2f11b36ca50e04af
Ilya Mandel <ilya.mandel@monash.edu>
2025-07-01 23:07:41 +1000

    First complete Matlab upload

--- compas_matlab_utils/SSEDetailedOutput.m
+++ compas_matlab_utils/SSEDetailedOutput.m
@@ -16,8 +16,9 @@ function SSEDetailedOutput(filename, nfiles)
 %       
 
 
-if(nargin==1)
+if(nargin==1) %parse one file
 
+file=filename;
 time=h5read(file,'/Time');
 Z=h5read(file,'/Metallicity@ZAMS');
 mass=h5read(file,'/Mass');
@@ -29,15 +30,20 @@ radius=h5read(file,'/Radius');
 temperature=h5read(file,'/Teff');
 mdot=h5read(file,'/Mdot');
 ml=h5read(file,'/Dominant_Mass_Loss_Rate');
-envMass=h5read(file,'/Mass_Env');
-convEnvMass=h5read(file,'/Mass_Convective_Env');
-lambda=h5read(file,'/Lambda_Convective');
-binding=h5read(file,'/BE_ConvectiveEnvelope');
+%envMass=h5read(file,'/Mass_Env');
+%convEnvMass=h5read(file,'/Mass_Convective_Env');
+%lambda=h5read(file,'/Lambda_Convective');
+%binding=h5read(file,'/BE_ConvectiveEnvelope');
 rec=h5read(file,'/Record_Type');
 
 
+figure(1),     %HR diagram
+scatter(temperature, luminosity, 20, type, 'filled');
+set(gca,'FontSize',20); xlabel('Teff, T_o'); ylabel('Luminosity, L_o'); set(gca,'XDir','reverse')
+H=colorbar; H.Label.String='Stellar type';
 
-figure(1), 
+
+figure(2),     %Radial expansion by phase
 scatter(time(type==1),radius(type==1),20,'filled', 'b'); hold on;
 scatter(time(type==2),radius(type==2),20,'filled', 'g');
 scatter(time(type==3),radius(type==3),20,'filled', 'y');
@@ -47,41 +53,39 @@ scatter(time(type==6),radius(type==6),20,'filled', 'k');
 hold off;
 set(gca,'FontSize',20); xlabel('Time, Myr'); ylabel('Radius, Rsun'), legend('MS','HG','FGB','CHeB','EAGB','TPAGB')
 
-for(i=2:length(radius)), %remove points where radius is less than the radius at previous time 
-    if(~isempty(find(radius(1:i-1)>=radius(i)))),
-        envMass(i)=0; convEnvMass(i)=0; radius(i)=0;
-    end;
-end;
+%for(i=2:length(radius)), %remove points where radius is less than the radius at previous time 
+%    if(~isempty(find(radius(1:i-1)>=radius(i)))),
+%        envMass(i)=0; convEnvMass(i)=0; radius(i)=0;
+%    end;
+%end;
+%goodindices=type>1 & type<=6 & radius>0;
+%figure(2); 
+%subplot(2,1,1), scatter(radius(goodindices), envMass(goodindices)-convEnvMass(goodindices), 20, 'b', 'filled'); hold on;
+%scatter(radius(goodindices), convEnvMass(goodindices), 20, 'm', 'filled'); hold on;
+%scatter(radius(goodindices), mass(goodindices)-envMass(goodindices), 20, 'y', 'filled'); 
+%scatter(radius(goodindices), mass(goodindices), 20, 'k', 'filled'); 
+%hold off;
+%set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('Mass, Msun'), legend('Radiative intershell','Convective Envelope','Core', 'Total')
+%subplot(2,1,2); scatter(radius(goodindices), binding(goodindices), 20, 'm', 'filled')
+%set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('Binding E conv env, erg')
+%%subplot(2,1,2); scatter(radius(goodindices), lambda(goodindices), 20, 'm', 'filled')
+%%set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('\lambda')
 
 
-goodindices=type>1 & type<=6 & radius>0;
-figure(2); 
-subplot(2,1,1), scatter(radius(goodindices), envMass(goodindices)-convEnvMass(goodindices), 20, 'b', 'filled'); hold on;
-scatter(radius(goodindices), convEnvMass(goodindices), 20, 'm', 'filled'); hold on;
-scatter(radius(goodindices), mass(goodindices)-envMass(goodindices), 20, 'y', 'filled'); 
-scatter(radius(goodindices), mass(goodindices), 20, 'k', 'filled'); 
-hold off;
-set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('Mass, Msun'), legend('Radiative intershell','Convective Envelope','Core', 'Total')
-subplot(2,1,2); scatter(radius(goodindices), binding(goodindices), 20, 'm', 'filled')
-set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('Binding E conv env, erg')
-%subplot(2,1,2); scatter(radius(goodindices), lambda(goodindices), 20, 'm', 'filled')
-%set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('\lambda')
-
-
-radius=h5read(file,'/BSE_Common_Envelopes/Radius(1)<CE');
-apre=h5read(file,'/BSE_Common_Envelopes/SemiMajorAxis<CE');
-a1=h5read(file,'/BSE_Common_Envelopes/SemiMajorAxisStage1>CE');
-apost=h5read(file,'/BSE_Common_Envelopes/SemiMajorAxis>CE');
-primarydonor=(h5read(file,'/BSE_Common_Envelopes/RLOF(1)')==1);
-figure(3); semilogy(radius(primarydonor), apre(primarydonor), 'y-.', radius(primarydonor), a1(primarydonor), 'y--', radius(primarydonor), apost(primarydonor), 'y-', 'LineWidth', 3); hold off;
-set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('Semimajor axis, Rsun'), legend('Pre CE','After Stage1','After Stage 2')
+%radius=h5read(file,'/BSE_Common_Envelopes/Radius(1)<CE');
+%apre=h5read(file,'/BSE_Common_Envelopes/SemiMajorAxis<CE');
+%a1=h5read(file,'/BSE_Common_Envelopes/SemiMajorAxisStage1>CE');
+%apost=h5read(file,'/BSE_Common_Envelopes/SemiMajorAxis>CE');
+%primarydonor=(h5read(file,'/BSE_Common_Envelopes/RLOF(1)')==1);
+%figure(3); semilogy(radius(primarydonor), apre(primarydonor), 'y-.', radius(primarydonor), a1(primarydonor), 'y--', radius(primarydonor), apost(primarydonor), 'y-', 'LineWidth', 3); hold off;
+%set(gca,'FontSize',20); xlabel('Radius, Rsun'); ylabel('Semimajor axis, Rsun'), legend('Pre CE','After Stage1','After Stage 2')
 
 end; % end of if(nargin==1)
 
 if(nargin==2),
 
 for (k=0:nfiles-1),
-file=['~/Work/COMPAS/src/COMPAS_Output_3/Detailed_Output/SSE_Detailed_Output_',int2str(k),'.h5'];    
+file=[filename,int2str(k),'.h5'];    
 time=h5read(file,'/Time');
 Z=h5read(file,'/Metallicity@ZAMS');
 mass=h5read(file,'/Mass');
