3e77a91c825fb2e2ecb66c3614c8d29f4c1e8513
Reinhold Willcox <reinhold.willcox@gmail.com>
2025-08-18 11:31:14 +0200

    changelog

--- src/changelog.h
+++ src/changelog.h
@@ -1650,6 +1650,9 @@
 //  03.23.01  IM - August 18, 2025      - Enhancement, defect repair:
 //                                          - In the MALTSEV SN prescription, treat wind-stripped stars as if they experienced case B mass transfer
 //                                          - Limit the output of CalculateEtaPTY() [Helium accretion efficiency onto WDs from Piersanti+ 2014, A3] to be in [0,1]
+//  03.23.02 RTW - August 18, 2025      - Enhancement:
+//                                          - Updated Maltsev SN prescription, to include Maltsev mode (extrapolation variant outside of Z bounds), 
+//                                            fallback option, fixed remnant mass, and added lum and teff as attributes of RLOFProperties
 //
 //
 // Version string format is MM.mm.rr, where
