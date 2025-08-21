0809c374d69e011155cbc81892f223d5032d11a4
Jeff Riley <jeff.riley@monash.edu>
2025-08-21 15:47:34 +1000

    Fix changelog comments

--- src/changelog.h
+++ src/changelog.h
@@ -1660,8 +1660,8 @@
 //                                          - Changed utils::GetGSLVersion() to avoid compiler warning "warning: ignoring attributes on template argument ‘int (*)(FILE*)’ [-Wignored-attributes]"
 //                                          - Reverted Makefile line "SOURCES := $(wildcard *.cpp)" to listing actual source files
 //  03.25.02  JR - August 20, 2025      - Defect repairs:
-//                                          - Corrected calculations for Hurley Gamma constant C (C_GAMMA - see Hurley et al. 2000, just after eq 23, should use a(75) <= 1.0, not a(75) == 1.0 - confirmed in BSE Fortran source)
 //                                          - Clamped B_GAMMA to [0.0, B_GAMMA] (per discussion just after eq 23 - confirmed in BSE Fortran source)
+//                                          - Corrected calculation for Hurley Gamma constant C (C_GAMMA - see Hurley et al. 2000, just after eq 23, should use a(75) <= 1.0, not a(75) == 1.0 - confirmed in BSE Fortran source)
 //
 // Version string format is MM.mm.rr, where
 //
