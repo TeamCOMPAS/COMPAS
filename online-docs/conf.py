1887f1d0c97897309a5839f2ac2b95ce76220d07
Jeff Riley <jeff.riley@monash.edu>
2026-03-09 10:11:03 +1100

    Add 'Last updated' timestamp to index page

--- online-docs/conf.py
+++ online-docs/conf.py
@@ -109,6 +109,13 @@ html_theme_options = {
     'show_relbar_bottom': 'true'
 }
 
+# last updated stamp
+# (not supported yet in alabster...) - use "today" in MD file for now
+html_last_updated_fmt = ""
+html_last_updated_use_utc = True
+
+today_fmt = '%Y-%m-%d %H:%M (UTC%z)'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
